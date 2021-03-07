19f730926047e1119b1d97237c8fb0f6f418c7e0
Nazhim <nazhimkalam@gmail.com>
2021-03-07 16:59:54 +0530

    added additional information explaining how it was obtained

--- w1761265/src/FordFulkerson.java
+++ w1761265/src/FordFulkerson.java
@@ -8,17 +8,18 @@ public class FordFulkerson {
 
         // Variables
         tot_Vertex = target + 1;
-        int x_index, y_index;
+        int x, y;
         int[][] resGraph = new int[tot_Vertex][tot_Vertex];
 
         // We are creating a residual graph by filling the residual graph with the given capacities from the original graph.
-        // In the Residual graph resGraph[x_index][y_index] indicates the residual capacity of edge from x_index to y_index.
-        // If rGraph[i][j] is 0, then there is No edge Else if its not 0 then there is an Edge
-        for (x_index = 0; x_index < tot_Vertex; x_index++) {
-            for (y_index = 0; y_index < tot_Vertex; y_index++) {
-                resGraph[x_index][y_index] = data[x_index][y_index];
+        // In the Residual graph resGraph[x][y] indicates the residual capacity of edge from x to y.
+        // If rGraph[i][j] is 0, then there is No edge Else if its not 0 then there IS AN Edge
+        for (x = 0; x < tot_Vertex; x++) {
+            for (y = 0; y < tot_Vertex; y++) {
+                resGraph[x][y] = data[x][y];
             }
         }
+        System.out.println("Creating the residual graph...");
 
         // The parent_arr is used to store the path and is filled by BFS
         int[] parent_arr = new int[tot_Vertex];
@@ -28,26 +29,31 @@ public class FordFulkerson {
 
         // Updating the residual values of edges by augmenting the flow while there is a path from source to sink.
         while (bfs(resGraph, source, target, parent_arr)) {
+            System.out.println("--->");
 
             // By using BFS we are finding the minimum residual capacity of the edges along the path which can be filled with.
             int path_flow = Integer.MAX_VALUE;
 
-            for (y_index = target; y_index != source; y_index = parent_arr[y_index]) {
-                x_index = parent_arr[y_index];
-                path_flow = Math.min(path_flow, resGraph[x_index][y_index]);
+            for (y = target; y != source; y = parent_arr[y]) {
+                x = parent_arr[y];
+                path_flow = Math.min(path_flow, resGraph[x][y]);
             }
+            System.out.println("Find the minimum residual capacity...");
 
             // Updating the residual capacities of the edges.
             // Also reversing the edges along the path.
-            for (y_index = target; y_index != source; y_index = parent_arr[y_index]) {
-                x_index = parent_arr[y_index];
-                resGraph[x_index][y_index] -= path_flow;
-                resGraph[y_index][x_index] += path_flow;
+            for (y = target; y != source; y = parent_arr[y]) {
+                x = parent_arr[y];
+                resGraph[x][y] -= path_flow;
+                resGraph[y][x] += path_flow;
             }
+
+            System.out.println("Path flow value found equal to " + path_flow);
 
             // Adding the path flows to the overall flow
             maximum_flow += path_flow;
         }
+        System.out.println("--->");
 
         // Returning the overall flow, which is also the maximum flow of the graph
         return String.valueOf(maximum_flow);
